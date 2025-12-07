d1272bb229674cf2e7a143f8ed170e4a394b6a5b
Traian <traianflorianalin@gmail.com>
2025-12-07 23:53:11 +0200

    Implemented the main skeleton of the 3rd problem

--- checker/problema3.c
+++ checker/problema3.c
@@ -27,21 +27,16 @@ char * itoa(int nr)
     } while (power);
     return rez;
 }
-char* codificare1(char *password)
+char* codificare1(char *password,char new_char)
 {
     char *res=calloc(1000,sizeof(char));
-    // printf("%d",strlen(res));
-    int fr[130]={0};
+    static int fr[130]={0};
 
     int i;
-    for(i=0;password[i]!='\0';i++)
-    {
 
-        int len = strlen(res);
-        res[len] = password[i];
-        strcat(res,itoa(fr[password[i]]));
-        fr[password[i]]++;
-    }
+    res[0]=new_char;
+    strcat(res,itoa(fr[new_char]));
+    fr[new_char]++;
     res[strlen(res)]='\0';
     return res;
 }
@@ -55,13 +50,12 @@ int nr_biti_setati(int nr)
     }
     return cnt;
 }
-char* codificare2(char *password)
+char* codificare2(char new_char)
 {
-    char *rez = calloc(2*strlen(password),sizeof(char));
+    char *rez = calloc(2,sizeof(char));
     int i;
-    for(i=0;password[i]!='\0';i++)
     {
-        char ch = password[i];
+        char ch = new_char;
         ch^=(1<<3);
         ch^=(1<<6);
 
@@ -81,18 +75,50 @@ char swap_letter_case(char ch)
         return ch+32;
     return ch;
 }
-char* codificare3(char *password)
+char* codificare3(char new_char)
 {
 
-    char *rez = calloc(2*strlen(password),sizeof(char));
+    char *rez = calloc(1,sizeof(char));
     int i;
-    for(i=0;password[i]!='\0';i++)
-        rez[i]=swap_letter_case(password[i]);
+    rez[0]=swap_letter_case(new_char);
     return rez;
 }
 int main()
 {
-    char *a = "Pula Mea";
-    printf("%s\n",codificare3(a));
+    int seed,L;
+    scanf("%d%d",&seed,&L);
+
+
+    char *generated_array=malloc(L*sizeof(char));
+    char codified_array[10000];
+
+    srand(seed);
+    char chars[]={')','\\','F','C'};
+    int steps[]={0,2,2,1};
+    for(int i=0;i<L;i++)
+    {
+        char c = chars[i];
+        // char c = rand()%(125-32)+32;
+        int codif = steps[i];
+        char *codification;
+        switch(codif)
+        {
+            case 0:
+                codification = strdup(codificare1(generated_array,c));
+                break;
+            case 1:
+                codification = strdup(codificare2(c));
+                break;
+            case 2:
+                codification = strdup(codificare3(c));
+                break;
+            default:
+                break;
+        }
+        // printf("%s ",codification);
+        strcat(codified_array,codification);
+        generated_array[i]=c;
+    }
+    printf("%s",generated_array);
     return 0;
 }
